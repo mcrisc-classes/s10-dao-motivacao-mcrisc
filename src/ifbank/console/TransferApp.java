1ddf8b6f10d2094457cccc4e2acb775fdafb2507
Professor LDA2 <professor@lda2.arq.ifsp.edu.br>
2022-09-30 11:10:40 -0300

    consulta nomes
    Consulta a nomes de clientes.

--- src/ifbank/console/TransferApp.java
+++ src/ifbank/console/TransferApp.java
@@ -1,7 +1,13 @@
 package ifbank.console;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Locale;
 import java.util.Scanner;
+
+import edu.ifsp.ifbank.ConnectionProvider;
 
 public class TransferApp {
 	private static final Locale BRAZIL = new Locale("pt", "br");
@@ -13,59 +19,86 @@ public class TransferApp {
 		char keepRunning = 'S';
 		
 		
-		/* TODO: abrir uma conexão com o banco de dados 
-		 * A conexão deve ficar aberta enquanto o programa estiver sendo executado.
-		 */		
-		
-		Scanner sc = new Scanner(System.in);
-		sc.useLocale(BRAZIL);			
-		
-		while (keepRunning == 'S') {
-			System.out.println("Transferências on-line");
-			
-			System.out.print("Conta de origem: ");
-			contaOrigem = sc.nextInt();
-			
-			System.out.print("Conta de destino: ");
-			contaDestino = sc.nextInt();
+		try (Connection conn = ConnectionProvider.getConnection()) {
+			conn.setAutoCommit(false);
 
-			System.out.print("Valor (formato: 0,00): ");
-			valor = sc.nextDouble();
+		
+			Scanner sc = new Scanner(System.in);
+			sc.useLocale(BRAZIL);			
 			
-			/* TODO: buscar nome do titular
-			 * - Use a variável `contaOrigem` para buscar o nome do titular da conta no banco de dados
-			 * - Se a conta não existir, mostre uma mensagem de erro e reinicie o programa (basta 
-			 * executar o comando `continue`, para para voltar ao início `while`).
-			 */
-			String titularOrigem = "titular1";
-			
-			/* TODO: buscar nome do titular
-			 * - Use a variável `contaDestino` para buscar o nome do titular da conta no banco de dados
-			 * - Se a conta não existir, mostre uma mensagem de erro e reinicie o programa.
-			 */
-			String titularDestino = "titular2";
-			
-			
-			System.out.println();
-			System.out.println("== Transferindo ==");
-			System.out.printf("%s -> %s\n", titularOrigem, titularDestino);
-			System.out.println("Valor: " + String.format(BRAZIL, "%.2f", valor));
-			
-			/* TODO: realizar transferência
-			 * Execute aqui o procedimento para realizar a transação que representa a transferência bancária
-			 */
+			while (keepRunning == 'S') {
+				System.out.println("Transferências on-line");
+				
+				System.out.print("Conta de origem: ");
+				contaOrigem = sc.nextInt();
+				
+				System.out.print("Conta de destino: ");
+				contaDestino = sc.nextInt();
+	
+				System.out.print("Valor (formato: 0,00): ");
+				valor = sc.nextDouble();
+				
+				/* TODO: buscar nome do titular
+				 * - Use a variável `contaOrigem` para buscar o nome do titular da conta no banco de dados
+				 * - Se a conta não existir, mostre uma mensagem de erro e reinicie o programa (basta 
+				 * executar o comando `continue`, para para voltar ao início `while`).
+				 */
+				String titularOrigem = "titular1";
+				
+				
+				
+				/* TODO: buscar nome do titular
+				 * - Use a variável `contaDestino` para buscar o nome do titular da conta no banco de dados
+				 * - Se a conta não existir, mostre uma mensagem de erro e reinicie o programa.
+				 */
+				String titularDestino = "titular2";
 
+				try (PreparedStatement ps = conn.prepareStatement(
+						"select p.nome from pessoa p inner join conta c on c.titular = p.id where c.numero = ?;")) {
+
+					titularOrigem = consultarCliente(ps, contaOrigem);
+					titularDestino = consultarCliente(ps, contaDestino);
+				}
+				
+				
+				System.out.println();
+				System.out.println("== Transferindo ==");
+				System.out.printf("%s -> %s\n", titularOrigem, titularDestino);
+				System.out.println("Valor: " + String.format(BRAZIL, "%.2f", valor));
+				
+				/* TODO: realizar transferência
+				 * Execute aqui o procedimento para realizar a transação que representa a transferência bancária
+				 */
+	
+				
+				System.out.print("Continuar [S/N]? ");
+				keepRunning = sc.next().toUpperCase().charAt(0);
+			}
 			
-			System.out.print("Continuar [S/N]? ");
-			keepRunning = sc.next().toUpperCase().charAt(0);
+			sc.close();
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		
-		sc.close();
 		
 		/* TODO: feche a conexão com o banco de dados
 		 */
 		
 		System.out.println("-- fim --");
+	}
+
+	private String consultarCliente(PreparedStatement ps, int numeroConta) throws SQLException {
+		String nome = null;
+		
+		ps.setInt(1, numeroConta);					
+		try (ResultSet rs = ps.executeQuery();) {
+			if (rs.next()) {
+				nome = rs.getString("nome");
+			}
+		}
+		
+		return nome;
 	}
 	
 	public static void main(String[] args) {
